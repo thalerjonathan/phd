25bd129a7059fef5de69727617814613e749c3d2
jonathan thaler <thalerjonathan>
2016-10-10 15:08:05 +0100

    moved some comments into the source-file

--- coding/thesis/sir/src/Main.hs
+++ coding/thesis/sir/src/Main.hs
@@ -22,6 +22,18 @@ import Control.Concurrent.STM.TChan
 
 - want to have proactivity, but how? e.g. calling regularly a function after having polled the message-box?
 
+- is it possible to use deterministic parallelism instead of classic concurrency e.g. transparent parallel execution and communication of data? would resemble more a pure functional style and allows to reason about programs
+
+3 approaches: 1. no parallelism/concurrency, 2. deterministic parallelism, 3. indeterministic concurrency
+
+what about ignoring parallelism and concurrency for now and just iterate round robin through agents?
+
+experiment with basic forkIO, MVar and STM: two threads communicating. install threadscope and look at execution
+
+instead of function to forkIO: use lambda which accesses data from outside forkio
+
+- but in all cases: use IVar, MVar or STM to "communicate" among agents: need some way of sharing data with each other. question: what would be the alternative?
+
 - WANT TO RETAIN ABILITY TO REASON ABOUT PROGRAMM DESPITE USING PARALLELISM AND CONCURRENCY
 - ALL HAS TO BE TYPESAFE (but this is ensured by Haskell anyway)
 -}

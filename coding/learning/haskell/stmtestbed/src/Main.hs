e815e70c585af719f0d882496335d2b9ef047d59
Jonathan Thaler <thaler.jonathan@gmail.com>
2016-11-01 09:29:43 +0000

    added stuff to geneis-paper and in STM-testcode

--- coding/learning/haskell/stmtestbed/src/Main.hs
+++ coding/learning/haskell/stmtestbed/src/Main.hs
@@ -4,6 +4,7 @@ import Control.Concurrent
 import Control.Concurrent.STM
 import Debug.Trace
 
+-------------------------------------------------------------------------------------------------------------
 data MessageType d = AgentStart | AgentStop | AgentDomain d deriving (Show, Eq)
 
 data Message d = Message
@@ -15,20 +16,42 @@ data Message d = Message
 data TestDomain = A | B | C deriving (Show, Eq)
 type STMTestbedMsg = Message TestDomain
 
-putMessage :: Message d -> TChan (Message d) -> STM ()
-putMessage msg box = writeTChan box msg
-
-readMessages :: (Show d) => TChan (Message d) -> STM ()
+readMessages :: (Show d) => TChan (Message d) -> IO ()
 readMessages box = do
-  ret <- tryReadTChan box
+  ret <- atomically $ tryReadTChan box
   case (ret) of Nothing -> return ()
                 Just msg -> trace (show msg) (readMessages box)
 
-testSTM :: Int -> STM ()
-testSTM n = do msgBox <- newTChan
-               writeTChan msgBox (Message { msgType = AgentStart, msgContent = Nothing })
-               writeTChan msgBox (Message { msgType = AgentStop, msgContent = Nothing })
-               writeTChan msgBox (Message { msgType = AgentDomain A, msgContent = Just "Domain Message A" })
-               readMessages msgBox
-          
-          
+createMsgBox :: IO (TChan (Message d))
+createMsgBox = newTChanIO
+
+main :: IO ()
+main = do msgBox <- createMsgBox
+          atomically $ writeTChan msgBox (Message { msgType = AgentStart, msgContent = Nothing })
+          atomically $ writeTChan msgBox (Message { msgType = AgentStop, msgContent = Nothing })
+          atomically $ writeTChan msgBox (Message { msgType = AgentDomain A, msgContent = Just "Domain Message A" })
+          readMessages msgBox
+-------------------------------------------------------------------------------------------------------------
+
+type FuncMsg = (Int -> Int)
+
+createFuncMsgBox :: IO (TChan FuncMsg)
+createFuncMsgBox = newTChanIO
+
+createMsgFunc :: Int -> (Int -> Int)
+createMsgFunc n = (\x -> n*x)
+
+readFuncMessages :: TChan FuncMsg -> IO ()
+readFuncMessages box = do
+  ret <- atomically $ tryReadTChan box
+  case (ret) of Nothing -> return ()
+                Just f -> trace (show (f 10)) (readFuncMessages box)
+                
+main' :: IO ()
+main' = do msgBox <- createFuncMsgBox
+           atomically $ writeTChan msgBox (createMsgFunc 1)
+           atomically $ writeTChan msgBox (createMsgFunc 2)
+           atomically $ writeTChan msgBox (createMsgFunc 3)
+           readFuncMessages msgBox
+
+-------------------------------------------------------------------------------------------------------------

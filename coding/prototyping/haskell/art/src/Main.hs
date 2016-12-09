3ca4e9e7eb788f4a0c8c036019e3959ce01032d9
thalerjonathan <thaler.jonathan@gmail.com>
2016-12-09 21:30:00 +0000

    implemented real circle-intersection which is the base for describing the islamic art

--- coding/prototyping/haskell/art/src/Main.hs
+++ coding/prototyping/haskell/art/src/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
+import Debug.Trace
+
 import qualified ARTRendering as Render
 import qualified Graphics.Gloss as GLO
 
 -- NOTE: order is x, y, r
-data Circle = Circle Float Float Float
+data Circle = Circle Float Float Float deriving (Show)
 
-data Point = Point Float Float
+data Point = Point Float Float deriving (Show)
 
 main :: IO ()
 main = render generateDescription
@@ -20,15 +22,18 @@ render cs = GLO.display
 toGlossPicture :: Circle -> GLO.Picture
 toGlossPicture (Circle x y r) = Render.drawCircleAt x y r
 
+circleFromPoint :: Float -> Point -> Circle
+circleFromPoint r (Point x y) = Circle x y r
+
 generateDescription :: [Circle]
-generateDescription = [initC, c1, c2, c3, c4]
+generateDescription = [c1, c2, c3, c4, c5]
     where
-        initC = Circle 0 0 80
-        c1 = left initC
-        c2 = right initC
-        c3 = top initC
-        c4 = bottom initC
-        c5 = centerOfTwo c1 c2
+        r = 80
+        c1 = Circle 0 0 r
+        c2 = right c1
+        (c3, c4) = intersect c1 c2
+        (c5, _) = intersect c1 c3
+        (c6, _) = intersect c1 c4
 
 left :: Circle -> Circle
 left (Circle x y r) = Circle (x - r) y r
@@ -42,9 +47,43 @@ top (Circle x y r) = Circle x (y + r) r
 bottom :: Circle -> Circle
 bottom (Circle x y r) = Circle x (y - r) r
 
-
-centerOfTwo :: Circle -> Circle -> Circle
-centerOfTwo (Circle x1 y1 r1) (Circle x2 y2 r2) = Circle xHalf y r1
+-- NOTE: this assumes there are exactly 2 intersection-points and r0 == r1
+intersect :: Circle -> Circle -> (Circle, Circle)
+intersect c0@(Circle x0 y0 r0) c1@(Circle x1 y1 r1) = (ci1, ci2)
     where
-        xHalf = (x2 - x1) / 2
-        y = sqrt((3 * r1 * r1) / 4)
+        r = r0
+        pis = intersectRaw c0 c1
+        (p1, p2) = rawIntersectionToPair pis
+        ci1 = circleFromPoint r p1
+        ci2 = circleFromPoint r p2
+
+
+rawIntersectionToPair :: Maybe [Point] -> (Point, Point)
+rawIntersectionToPair Nothing = undefined
+rawIntersectionToPair (Just (p1:p2:ps)) = (p1, p2)
+
+intersectionsToCircles :: Maybe [Point] -> Float -> [Circle]
+intersectionsToCircles Nothing _ = []
+intersectionsToCircles (Just pis) r = map (circleFromPoint r) pis
+
+-- NOTE: taken from http://paulbourke.net/geometry/circlesphere/tvoght.c
+intersectRaw :: Circle -> Circle -> Maybe [Point]
+intersectRaw (Circle x0 y0 r0) (Circle x1 y1 r1)
+    | d == 0 = Nothing        -- OVERLAY, same center, infinite number solutions
+    | d > (r0 + r1) = Nothing            -- SEPARATE, no solutions
+    | d < abs (r0 - r1) = Nothing        -- CONTAINS, no solutions
+    | d == (r0 + r1) = Just [Point x2 y2]         -- INTERSECTION in single point
+    | otherwise = Just [Point xi1 yi1, Point xi2 yi2]   -- INTERSECTION: two solutions
+    where
+        (dx, dy) = (x1 - x0, y1 - y0)
+        d = sqrt(dx*dx + dy*dy)
+        a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d)
+        h = sqrt((r0*r0) - (a*a))
+        x2 = x0 + (dx * a/d)
+        y2 = y0 + (dy * a/d)
+        rx = -dy * (h/d)
+        ry = dx * (h/d)
+        xi1 = x2 + rx
+        yi1 = y2 + ry
+        xi2 = x2 - rx
+        yi2 = y2 - ry

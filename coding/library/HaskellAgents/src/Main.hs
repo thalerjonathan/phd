cc0ebd5f102717264c19647d169addcb629fca44
thalerjonathan <thaler.jonathan@gmail.com>
2016-12-16 03:13:21 +0000

    added new project: HaskellAgents. This will be a proper implementation of a library for ABM/S in Haskell

---
+++ coding/library/HaskellAgents/src/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+main :: IO ()
+main = do
+    let b = makeOffer bid
+    let r = executeOffer ask b
+    putStrLn (show r)
+        where
+            bid = Bid 42
+            ask = Ask 43
+
+data Offering a = Bid a | Ask a | Accept | Refuse deriving (Show)
+
+data Conversation m = Msg m | Conv (m -> Conversation m)
+
+{- NOTE: this part sends an offer enclosed in the lambda and will compare it to a passed in offer where a reply is then made
+         TODO: introduce additional step> send o along with an additional reply and wait
+-}
+makeOffer :: Offering Int -> Conversation (Offering Int)
+makeOffer o = Conv (\o' -> if compareOffer o o' then
+                            Msg Accept
+                                else
+                                    Msg Refuse )
+
+{- NOTE: this part is the counterpart which allows to execute a given offering-conversation with a given offering
+         problem: we loose track were we are like in makeOffer
+         TODO: must always terminate and finally return Message
+
+-}
+executeOffer :: Offering Int -> Conversation (Offering Int) -> Offering Int
+executeOffer o (Conv f) = executeOffer o (f o)
+executeOffer o (Msg o') = o'
+
+compareOffer :: Offering Int -> Offering Int -> Bool
+compareOffer (Bid a) (Ask a') = a >= a'
+compareOffer (Ask a) (Bid a') = a <= a'
+compareOffer _ _ = False

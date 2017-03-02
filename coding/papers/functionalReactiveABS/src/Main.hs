68ac561bed60538bdf1ef859b01ad52b693a57ff
Jonathan Thaler <thaler.jonathan@gmail.com>
2017-03-02 16:53:46 +0000

    started working on functional reactive ABS

--- coding/papers/functionalReactiveABS/src/Main.hs
+++ coding/papers/functionalReactiveABS/src/Main.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import Agent.Agent
+
 main :: IO ()
-main = putStrLn "Functional Reactive ABS"
+main = do
+        let as =
+
+type TestAgentMsg = Increment | Decrement
+type TestAgentState = Int
+type TestAgent = Agent TestAgentState TestAgentMsg
+type TestAgentBehaviour = AgentBehaviour TestAgentState TestAgentMsg
+
+createAgents :: [TestAgent]
+createAgents = [a0, a1, a2]
+    where
+        a0 = TestAgent { adId = 0,
+                        adState = 0,
+                        adBehaviour = testAgentBehaviour a0 }
+
+        a1 = TestAgent { adId = 1,
+                        adState = 0,
+                        adBehaviour = testAgentBehaviour a1 }
+
+        a2 = TestAgent { adId = 2,
+                        adState = 0,
+                        adBehaviour = testAgentBehaviour a2 }
+
+testAgentBehaviour :: TestAgent -> TestAgentBehaviour
+testAgentBehaviour aInit = proc agentIn ->
+    do
+        returnA -< AgentOut{ aoState = (aiState agentIn) }

03a1026c66026564234c6d27d48f9f89657798d3
Jonathan Thaler <thaler.jonathan@gmail.com>
2017-03-03 11:30:24 +0000

    working on functional reactive ABS

--- coding/papers/functionalReactiveABS/src/Agent/Agent.hs
+++ coding/papers/functionalReactiveABS/src/Agent/Agent.hs
@@ -9,30 +9,142 @@ type AgentId = Int
 type AgentMessage m = (AgentId, m)
 type AgentBehaviour s m = SF (AgentIn s m) (AgentOut s m)
 
-data Agent s m = Agent {
+data AgentDef s m = AgentDef {
     adId :: AgentId,
     adState :: s,
     adBehaviour :: AgentBehaviour s m
 }
 
 data AgentIn s m = AgentIn {
+    aiId :: AgentId,
     aiMessages :: [AgentMessage m],     -- AgentId identifies sender
     aiStart :: Event (),
     aiStop :: Event (),
     aiTerminate :: Event(),
-    aiState :: s
+    aiState :: s,
+    aiBehaviour :: AgentBehaviour s m
 }
 
 data AgentOut s m = AgentOut {
     aoKill :: Event (),
-    aoCreate :: [Agent s m],
+    aoCreate :: [AgentDef s m],
     aoMessages :: [AgentMessage m],     -- AgentId identifies receiver
     aoState :: s
 }
 
+{-
+processIO :: [AgentDef s m] -> ([AgentOut s m] -> IO (Bool, Double)) -> IO ()
+processIO as outFunc = do
+                        hdl <- reactInit
+                                    (return as)
+                                    (iter outFunc)
+                                    (process)
+                                    Agent.Agent.iterate hdl (1.0, Nothing) -- TODO: need to get this dt correct instead of 0.5, should come from input/output
+                        return ()
+
+iterate :: ReactHandle a b -> (DTime, Maybe a) -> IO Bool
+iterate hdl (dt, input) = do
+    cont <- react hdl (1.0, Nothing)  -- TODO: need to get this dt correct instead of 0.5, should come from input/output
+    if cont then
+        Agent.Agent.iterate hdl (dt, input)
+            else
+                return False
+
+-- NOTE: don't care about a, we don't use it anyway
+iter :: ([AgentOut s m] -> IO (Bool, Double)) -> ReactHandle a [AgentOut s m] -> Bool -> [AgentOut s m] -> IO Bool
+iter outFunc hdl _ out = do
+                    (cont, dt) <- outFunc out
+                    return cont
+-}
+
+{- NOTE: to run Yampa in a pure-functional way use embed -}
+processSteps :: [AgentDef s m] -> Double -> Int -> [[AgentOut s m]]
+processSteps as dt steps = embed
+                            (process as)
+                            ([], sts)
+    where
+
+        -- NOTE: again haskells laziness put to use: take steps items from the infinite list of sampling-times
+        sts = take steps $ samplingTimes 0 dt
+
+-- NOTE: this creates an infinite list of sampling-times with starting time t and sampling-interval dt
+samplingTimes :: Double -> Double -> [(DTime, Maybe a)]
+samplingTimes t dt = (t', Nothing) : (samplingTimes t' dt)
+    where
+        t' = t + dt
+
+----------------------------------------------------------------------------------------------------------------------
+-- PRIVATES
+----------------------------------------------------------------------------------------------------------------------
+process :: [AgentDef s m] -> SF [AgentIn s m] [AgentOut s m]
+process initAds = proc _ ->
+    do
+        aos <- process' asfs ains -< []
+        returnA -< aos
+
+    where
+        ains = createStartingAgentIn initAds
+        asfs = map aiBehaviour ains
+
+createStartingAgentIn :: [AgentDef s m] -> [AgentIn s m]
+createStartingAgentIn as = map startingAgentInFromAgent as
+    where
+        startingAgentInFromAgent :: AgentDef s m -> AgentIn s m
+        startingAgentInFromAgent a = AgentIn { aiId = (adId a),
+                                                aiMessages = [],
+                                                aiStart = Event (),
+                                                aiStop = NoEvent,
+                                                aiTerminate = NoEvent,
+                                                aiState = (adState a),
+                                                aiBehaviour = (adBehaviour a)}
+
+process' :: [AgentBehaviour s m] -> [AgentIn s m] -> SF [AgentIn s m] [AgentOut s m]
+process' asfs ains = proc _ ->
+    do
+        aos <- dpSwitch
+                   route
+                   asfs
+                   (arr collectOutput >>> notYet)  -- NOTE: in the first iteration we don't fire yet
+                   feedBack -< ains
+        returnA -< aos
+
+
+route :: [AgentIn s m] -> [sf] -> [(AgentIn s m, sf)]
+route ains sfs = zip ains sfs
+
+-- TODO: distribute messages here
+collectOutput :: ([AgentIn s m], [AgentOut s m]) -> (Event ([AgentOut s m], [AgentIn s m]))
+collectOutput (oldAgentIn, newAgentOuts) = Event (newAgentOuts, newAgentIns)
+    where
+        newAgentIns = map (agentOutToAgentIn) (zip oldAgentIn newAgentOuts)
+
+        agentOutToAgentIn :: (AgentIn s m, AgentOut s m) -> AgentIn s m
+        agentOutToAgentIn (oldIn, newOut) = oldIn { aiStart = NoEvent,
+                                                    aiState = (aoState newOut) }
+
+-- TODO: add/remove signal-functions on agent creation/destruction
+feedBack :: [AgentBehaviour s m] -> ([AgentOut s m], [AgentIn s m]) -> SF [AgentIn s m] [AgentOut s m]
+feedBack asfs (newAgentOuts, newAgentIns) = process' asfs newAgentIns
+
 ----------------------------------------------------------------------------------------------------------------------
 -- EXPORTS
 ----------------------------------------------------------------------------------------------------------------------
+{-
+{- NOTE: Here we are pairing up (zip) the inputs to each signal-function. Each agents SF receives the (new) agent-state
+         itself and the (new) positions of the enemy and friend -}
+routeStartingAgent :: [AgentDef s m] -> [sf] -> [(AgentIn s m, sf)]
+routeStartingAgent as sfs = zip ains sfs
+    where
+        ains = createStartingAgentIn as
+
+startAgents :: [AgentDef s m] -> SF [AgentIn s m] [AgentOut s m]
+startAgents initAs = proc _ ->
+    do
+        aos <- par routeStartingAgent asfs -< initAs
+        returnA -< aos
+    where
+        asfs = map adBehaviour initAs
+
 processIO :: Sim.SimulationIO
 processIO simIn outFunc = do
                     hdl <- reactInit
@@ -75,31 +187,9 @@ samplingTimes :: Double -> Double -> [(DTime, Maybe a)]
 samplingTimes t dt = (t', Nothing) : (samplingTimes t' dt)
     where
         t' = t + dt
+-}
 
-----------------------------------------------------------------------------------------------------------------------
--- PRIVATES
-----------------------------------------------------------------------------------------------------------------------
-{- NOTE: This is just the initial signal-function which will be use ONCE to kick-off the whole simulation:
-         it just creates the signal-functions (one for each agent) and then passes them and the initial agent-states
-         to the process'.
-         SimOut is only used as an impostor for now, no real use but introduced for further usages when really necessary -}
-process :: [Agent] -> SF [AgentIn] [AgentOut]
-process initAs = proc _ ->
-    do
-        -- TODO: send start to all agents, this is done using par
-        -- TODO: from AgentOut and Agent, create new AgentIn
-        aos <- process' initASFs initAs -< []
-        returnA -< aos
-    where
-        initASFs = map adBehaviour initAs
-
-startAgents :: [Agent] -> SF [AgentIn] [AgentOut]
-startAgents initAs = proc _ ->
-    do
-        let agentIns =
-        aos <- par
-                    route
-
+{-
 process' :: [Agent] -> SF [AgentIn] [AgentOut]
 process' asfs ain = proc _ ->
     do
@@ -152,3 +242,4 @@ activeAgent initAgentDef = proc agentIn ->
         let ao = Agent.agentStep wt stepWidth agentIn
         -- let ao = trace ("time=" ++ (show t) ++ ", derivative=" ++ (show d) ++ "stepwidth=" ++ (show stepWidth)) Agent.agentStep stepWidth agentIn
         returnA -< ao
+-}
--- coding/papers/functionalReactiveABS/src/Main.hs
+++ coding/papers/functionalReactiveABS/src/Main.hs
@@ -1,32 +1,58 @@
+{-# LANGUAGE Arrows #-}
+
 module Main where
 
+import FRP.Yampa
+import Debug.Trace
 import Agent.Agent
+
+data TestAgentMsg = Increment | Decrement deriving (Show)
+type TestAgentState = Int
+type TestAgent = AgentDef TestAgentState TestAgentMsg
+type TestAgentBehaviour = AgentBehaviour TestAgentState TestAgentMsg
 
 main :: IO ()
 main = do
-        let as =
+        let as = createAgents
+        let as' = processSteps as 1.0 10
+        let lastAs' = last as'
+        putStrLn $ show (length lastAs')
+        mapM printAgentOut lastAs'
+        return ()
 
-type TestAgentMsg = Increment | Decrement
-type TestAgentState = Int
-type TestAgent = Agent TestAgentState TestAgentMsg
-type TestAgentBehaviour = AgentBehaviour TestAgentState TestAgentMsg
+
+printAgentOut :: (Show s) => AgentOut s m -> IO ()
+printAgentOut ao = do
+                    let s = aoState ao
+                    putStrLn $ "AgentOut: " ++ (show s)
 
 createAgents :: [TestAgent]
 createAgents = [a0, a1, a2]
     where
-        a0 = TestAgent { adId = 0,
+        a0 = AgentDef { adId = 0,
                         adState = 0,
                         adBehaviour = testAgentBehaviour a0 }
 
-        a1 = TestAgent { adId = 1,
-                        adState = 0,
+        a1 = AgentDef { adId = 1,
+                        adState = 1,
                         adBehaviour = testAgentBehaviour a1 }
 
-        a2 = TestAgent { adId = 2,
-                        adState = 0,
+        a2 = AgentDef { adId = 2,
+                        adState = 2,
                         adBehaviour = testAgentBehaviour a2 }
 
 testAgentBehaviour :: TestAgent -> TestAgentBehaviour
 testAgentBehaviour aInit = proc agentIn ->
     do
-        returnA -< AgentOut{ aoState = (aiState agentIn) }
+        let s = aiState agentIn
+        let s' = s + 1
+        let test = if (isEvent (aiStart agentIn) ) then
+                        trace ("Start-Event in " ++ (show (aiId agentIn))) " Start"
+                        else
+                            " NoStart"
+
+        returnA -< trace ("testAgentBehaviour in " ++ (show (aiId agentIn)) ++ (show test))
+                            (AgentOut{ aoKill = NoEvent,
+                                         aoCreate = [],
+                                         aoMessages = [],
+                                         aoState = s' })

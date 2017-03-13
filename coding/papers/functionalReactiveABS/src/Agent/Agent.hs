178f677064b1beaa0d774e2bd0bde71ce61a900f
Jonathan Thaler <thaler.jonathan@gmail.com>
2017-03-13 14:41:36 +0000

    added todos to FrABS

--- coding/papers/functionalReactiveABS/src/Agent/Agent.hs
+++ coding/papers/functionalReactiveABS/src/Agent/Agent.hs
@@ -34,6 +34,26 @@ data AgentOut s m = AgentOut {
 }
 
 ----------------------------------------------------------------------------------------------------------------------
+-- TODOs
+-- TODO implement sequential iteration
+-- TODO implement parallel iteration
+-- TODO random iteration in sequential
+-- TODO create project structure according to put it on Hackage in september: tests, comments,...
+-- TODO write unit-tests
+-- TODO write QuickCheck tests
+-- TODO need a way to specify the environment: spatial (2d/3d continuous/discrete) or graph. includes global neighbourhood information
+-- TODO  environment also needs a signalfunction which runs once per simulation-step: environmentIn/out
+-- TODO agents need to be able to access the environment and update it: sequential vs parallel
+-- TODO implement segregation
+-- TODO implement recursive simulations: MetaABS: test it using the segregation model. run recursive simulation for local neighbourhood only
+-- TODO implement SugarScape all chapters
+    -- TODO implement rules as SF which can be turned on or off
+    -- TODO formalize rules in my EDSL
+    -- TODO problem of sugarscape trading in our functional approach: cannot reply immediately thus potentially violating budget constraints. need to solve this e.g. by having a temporary reserved amount "open for transaction"
+-- TODO STM FrABS using Dunai?
+----------------------------------------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------------------------
 -- RUNNING SIMULATION WITH ITS OWN LOOP
 ----------------------------------------------------------------------------------------------------------------------
 processIO :: [AgentDef s m]

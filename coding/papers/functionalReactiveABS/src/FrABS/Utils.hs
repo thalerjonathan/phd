5a61e9c9b4453de5690ec82182a9176b75b2e17a
Jonathan Thaler <thaler.jonathan@gmail.com>
2017-03-14 16:56:05 +0000

    created runSF function which returns a SF instead of SF'

--- coding/papers/functionalReactiveABS/src/FrABS/Utils.hs
+++ coding/papers/functionalReactiveABS/src/FrABS/Utils.hs
@@ -5,6 +5,20 @@ import FRP.Yampa.InternalCore
 ------------------------------------------------------------------------------------------------------------------------
 -- Running a Signal-Function
 ------------------------------------------------------------------------------------------------------------------------
+runSF :: SF a b -> a -> (SF a b, b)
+runSF sf0 a0 = (SF {sfTF = tf0}, b0)
+    where
+        (sf', b0) = runSFInit sf0 a0
+
+        tf0 = (\_ -> (tf', b0))
+        tf' = runSFAux sf'
+
+        runSFAux sfCont  = SF' tf
+            where
+                tf dt i = (sf', b0)
+                    where
+                        (sf', b0) = runSFCont sfCont i dt
+
 -- This will run the given SF with the given input a and returns the continuation SF with the output b
 runSFInit :: SF a b -> a -> (SF' a b, b)
 runSFInit sf0 a0 = (sfTF sf0) a0

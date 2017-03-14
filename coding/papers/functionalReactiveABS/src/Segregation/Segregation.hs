5e80344ee6dda7ffd0265883b4ffd77f91a1d3f6
Jonathan Thaler <thaler.jonathan@gmail.com>
2017-03-14 14:00:29 +0000

    added notes

---
+++ coding/papers/functionalReactiveABS/src/Segregation/Segregation.hs
@@ -0,0 +1,3 @@
+module Segregation.Segregation where
+
+-- schelling segregation: for esch move compute 1 step recursive and see how this move performs. if it improves the outcome, then take it otherwise stay.

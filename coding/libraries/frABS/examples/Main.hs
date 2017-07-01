bdeb17912e7c851ca55dc5ad55eec561554d0f46
thalerjonathan <thaler.jonathan@gmail.com>
2017-07-01 19:06:22 +0100

    experimented with STM and unsafeperformIO to create a unique agent-id creator

--- coding/libraries/frABS/examples/Main.hs
+++ coding/libraries/frABS/examples/Main.hs
@@ -43,3 +43,53 @@ main = runFrSIRSSpatialWithRendering
     -- runConversationSteps
     -- runMetaABSStepsAndPrint
     -- runSegWithRendering
+
+{-
+import System.IO
+import System.IO.Unsafe
+import System.Random
+
+import Control.Monad
+import Control.Concurrent
+import Control.Monad.STM
+import Control.Concurrent.STM.TVar
+import Control.Concurrent.STM.Stats
+
+main :: IO ()
+main = do
+    hSetBuffering stdout NoBuffering
+
+    var <- newTVarIO 0
+    mapM (\i -> forkIO $
+        do 
+            threadId <- myThreadId
+            forever $
+                do
+                    randDelay <- getStdRandom (randomR (100000,500000))
+                    threadDelay randDelay
+
+                    -- value <- atomically $ incrementAtomically var
+                    --value <- trackSTM $ incrementAtomically var
+                    let value = incrementAtomicallyUnsafe var
+
+                    putStrLn ("Thread " ++ (show threadId) ++ " produced value: " ++ (show value))
+            ) [1..10]
+
+    -- wait 10 seconds
+    threadDelay 10000000
+
+    value <- readTVarIO var
+    putStrLn ("Main reads value: " ++ (show value))
+
+    --dumpSTMStats
+
+incrementAtomically :: TVar Int -> STM Int
+incrementAtomically var = 
+    do
+        value <- readTVar var
+        writeTVar var (value + 1)
+        return value
+
+incrementAtomicallyUnsafe :: TVar Int -> Int
+incrementAtomicallyUnsafe = unsafePerformIO  . atomically . incrementAtomically
+-}

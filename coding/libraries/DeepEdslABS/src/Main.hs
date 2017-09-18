28e39992642fc7c944df1ec518aff7327fb0f847
thalerjonathan <thaler.jonathan@gmail.com>
2017-09-18 18:07:58 +0100

    deep edsl abs mini prototype now working

--- coding/libraries/DeepEdslABS/src/Main.hs
+++ coding/libraries/DeepEdslABS/src/Main.hs
@@ -25,57 +25,81 @@ interpretAgent s ms ad' (OnAnyMessage t f)
 interpretAgent s ms ad' (SendMessage msg ad) = interpretAgent s ms' ad' ad
     where
         ms' = msg : ms
-
 interpretAgent s ms ad' (UpdateState f ad) = interpretAgent s' ms ad' ad
     where
         s' = f s
-
 interpretAgent s ms ad' (RepeatWith ad) = (s, ms, ad)
-
 interpretAgent s ms ad' Repeat = (s, ms, ad')
-
 interpretAgent s ms ad' DoNothing = (s, ms, DoNothing)
 
 main :: IO ()
 main = 
     do
-        {--
         let ad0 = OnAnyMessage
-                    (SendMessage (1, ()) (UpdateState (\s -> succ 1) Repeat)) 
+                    (SendMessage (1, ()) (UpdateState (\s -> succ s) Repeat)) 
                     (SendMessage (1, ()) Repeat)
         let ad1 = OnAnyMessage 
-                    (SendMessage (0, ()) (UpdateState (\s -> pred 1) Repeat)) 
+                    (SendMessage (0, ()) (UpdateState (\s -> pred s) Repeat)) 
                     (SendMessage (0, ()) Repeat)
-        --}
-
-        let ad0 = UpdateState (\s -> s + 1) Repeat
-        let ad1 = UpdateState (\s -> s - 1) Repeat
 
         let a0 = (0, 42, ad0)
         let a1 = (1, (-42), ad1)
 
         let as = [a0, a1]
-        let as' = runAgents 5000 as
+        --let as' = runAgentsSeq 5000 as
+        let as' = runAgentsPar 5000 as
 
-        mapM_ (\(_, s, _) -> print s) (head as')
+        mapM_ (\(aid, s, _) -> putStrLn $ show aid ++ ": " ++ show s) (head as')
         putStrLn ""
-        mapM_ (\(_, s, _) -> print s) (last as')
+        mapM_ (\(aid, s, _) -> putStrLn $ show aid ++ ": " ++ show s) (last as')
 
-runAgents :: Int -> [(AgentId, s, AgentDef s m)] -> [[(AgentId, s, AgentDef s m)]]
-runAgents n as = ass
+
+runAgentsPar :: Int 
+                -> [(AgentId, s, AgentDef s m)] 
+                -> [[(AgentId, s, AgentDef s m)]]
+runAgentsPar n as = ass
     where
-        (_, ass) = iterateAgents n as Map.empty
+        (_, ass) = iterateAgents n as [] Map.empty
 
         iterateAgents :: Int ->
                             [(AgentId, s, AgentDef s m)] -> 
+                            [[(AgentId, s, AgentDef s m)]] ->
                             (Map.Map AgentId [(AgentId, m)]) ->
                             (Map.Map AgentId [(AgentId, m)], [[(AgentId, s, AgentDef s m)]])
-        iterateAgents n as msgMap 
-            | n <= 0 = (msgMap, [as])
-            | otherwise = (msgMap'', as' : ass)
+        iterateAgents n as assAcc msgMap 
+            | n <= 0 = (msgMap, as : assAcc)
+            | otherwise = iterateAgents (n - 1) as' (as' : assAcc) msgMap'
+            where
+                asWithMsgs = map (iterateAgentsAux msgMap) as
+
+                msgMap' = foldr (\((aid, _, _), msgs) accMsgs -> distributeMessages aid msgs accMsgs) (Map.empty) asWithMsgs
+                as' = map fst asWithMsgs
+
+                iterateAgentsAux :: (Map.Map AgentId [(AgentId, m)]) ->
+                                        (AgentId, s, AgentDef s m) ->
+                                        ((AgentId, s, AgentDef s m), [(AgentId, m)])
+                iterateAgentsAux msgMap (aid, s, ad) = ((aid, s', ad'), sendMsgs)
+                    where
+                        mayRecvMsgs = Map.lookup aid msgMap
+                        recvMsgs = maybe [] id mayRecvMsgs
+
+                        (s', sendMsgs, ad') = interpretAgent s recvMsgs ad ad
+
+runAgentsSeq :: Int -> [(AgentId, s, AgentDef s m)] -> [[(AgentId, s, AgentDef s m)]]
+runAgentsSeq n as = ass
+    where
+        (_, ass) = iterateAgents n as [] Map.empty
+
+        iterateAgents :: Int ->
+                            [(AgentId, s, AgentDef s m)] -> 
+                            [[(AgentId, s, AgentDef s m)]] ->
+                            (Map.Map AgentId [(AgentId, m)]) ->
+                            (Map.Map AgentId [(AgentId, m)], [[(AgentId, s, AgentDef s m)]])
+        iterateAgents n as assAcc msgMap 
+            | n <= 0 = (msgMap, as : assAcc)
+            | otherwise = iterateAgents (n - 1) as' (as' : assAcc) msgMap'
             where
                 (msgMap', as') = foldr iterateAgentsAux (msgMap, []) as
-                (msgMap'', ass) = iterateAgents (n - 1) as' msgMap'
 
                 iterateAgentsAux :: (AgentId, s, AgentDef s m) -> 
                                         (Map.Map AgentId [(AgentId, m)], [(AgentId, s, AgentDef s m)]) ->

1b2721e2b9c0fc4f7afc6c470be33a273bceb585
thalerjonathan <thaler.jonathan@gmail.com>
2017-11-10 10:35:40 +0000

    added new file

---
+++ coding/papers/peerYufeng/repast/SocialForce/src/socialForce/SocialForceBuilder.java
@@ -0,0 +1,44 @@
+package socialForce;
+
+public class SocialForceBuilder implements ContextBuilder<Object> {
+
+	@Override
+	public Context build(Context<Object> context) {
+		context.setId("socialforce");
+
+		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder
+				.createContinuousSpaceFactory(null);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace(
+				"space", context, new RandomCartesianAdder<Object>(),
+				new repast.simphony.space.continuous.WrapAroundBorders(), 50,
+				50);
+
+		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
+		Grid<Object> grid = gridFactory.createGrid("grid", context,
+				new GridBuilderParameters<Object>(new WrapAroundBorders(),
+						new SimpleGridAdder<Object>(), true, 50, 50));
+
+		Parameters params = RunEnvironment.getInstance().getParameters();
+		int zombieCount = (Integer) params.getValue("zombie_count");
+		for (int i = 0; i < zombieCount; i++) {
+			context.add(new Zombie(space, grid));
+		}
+
+		int humanCount = (Integer) params.getValue("human_count");
+		for (int i = 0; i < humanCount; i++) {
+			int energy = RandomHelper.nextIntFromTo(4, 10);
+			context.add(new Human(space, grid, energy));
+		}
+
+		for (Object obj : context) {
+			NdPoint pt = space.getLocation(obj);
+			grid.moveTo(obj, (int) pt.getX(), (int) pt.getY());
+		}
+		
+		if (RunEnvironment.getInstance().isBatch()) {
+			RunEnvironment.getInstance().endAt(20);
+		}
+
+		return context;
+	}
+}

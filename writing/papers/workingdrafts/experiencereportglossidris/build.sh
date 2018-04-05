e53c4593fcf27cd4e530ee1162f69727a243cd40
thaler.jonathan <thaler.jonathan@gmail.com>
2018-04-05 16:10:27 +0200

    removed experience report of gloss-idris port

--- writing/papers/workingdrafts/experiencereportglossidris/build.sh
+++
@@ -1,8 +0,0 @@
-pdflatex -shell-escape -synctex=1 -interaction=nonstopmode epglossidris.tex
-wait
-bibtex epglossidris.aux
-wait
-pdflatex -shell-escape -synctex=1 -interaction=nonstopmode epglossidris.tex
-wait
-pdflatex -shell-escape -synctex=1 -interaction=nonstopmode epglossidris.tex
-wait

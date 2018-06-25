d9fdf4bd54a2e3db378bda4dff1c8f3f44bd9f92
Jonathan Thaler <thaler.jonathan@gmail.com>
2018-06-25 14:43:07 +0100

    working on sugarscape

--- public/towardsfunctionalabs/code/SugarScape/src/AgentMonad.hs
+++ public/towardsfunctionalabs/code/SugarScape/src/AgentMonad.hs
@@ -2,7 +2,16 @@
 module AgentMonad
   (
     AgentId
+
   , MonadAgent (..)
+
+  , Agent
+  , AgentCont
+  , AgentTX
+
+  , AgentDef (..)
+  , AgentIn (..)
+  , AgentOut (..)
 
   , mkAbsState
   ) where
@@ -37,4 +46,26 @@ mkAbsState :: ABSState
 mkAbsState = ABSState 
   { absNextId = 0
   , absTime   = 0
+  }
+
+-- an agent is simply a SF with a generic computational context, which depends on the model
+-- note that it is important that we do not fix m e.g. to StateT to allow an environment
+-- or adding a RandT for allowing randomness but we leave this to the model implementer, otherwise
+-- we would burden the API with details (type of the state in StateT, type of the RandomNumber generator 
+-- in RandT) they may not need e.g. there are models which do not need a global read/write environment
+-- or event don't use randonmness (e.g. SD emulation)
+type AgentCont m o = SF (MonadAgent m) AgentIn (AgentOut m o)
+type Agent m o     = AgentId -> (MonadAgent m) (AgentCont m o)
+
+data AgentDef m o = AgentDef
+  { adId       :: !AgentId
+  , adBeh      :: Agent m o
+  }
+
+data AgentIn = AgentIn { }
+
+data AgentOut m o = AgentOut 
+  { aoKill       :: !(Event ())
+  , aoCreate     :: ![AgentDef m o]
+  , aoObservable :: !(Maybe o)             -- OPTIONAL observable state
   }

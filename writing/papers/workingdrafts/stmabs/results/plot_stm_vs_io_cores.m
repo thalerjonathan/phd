b71ab63f80dababf2d9ec3f117b67c30b1ea0582
Jonathan Thaler <thaler.jonathan@gmail.com>
2018-07-12 15:34:24 +0100

    re-run 51x51 io 3 and 4 cores experiments

--- writing/papers/workingdrafts/stmabs/results/plot_stm_vs_io_cores.m
+++ writing/papers/workingdrafts/stmabs/results/plot_stm_vs_io_cores.m
@@ -1,11 +1,11 @@
 stm = [53.182, 27.817, 21.776, 20.201];
-io = [60.564, 42.779, 38.614, 41.914];
+io = [60.564, 42.779, 38.586, 41.555];
 cores = [1, 2, 3, 4];
 
 figure;
-plot (cores, stm, "linewidth", 2, "color", [0, 0.7, 0]);
+loglog (cores, stm, "linewidth", 2, "color", [0, 0.7, 0]);
 hold on;
-plot (cores, io, "linewidth", 2, "color", [0.7, 0, 0]);
+loglog (cores, io, "linewidth", 2, "color", [0.7, 0, 0]);
 
 legend ("STM", "IO");
 xlabel ("Cores");

443bf1f031e68e027bacd82ffc9e8cc0d3d3f16f
Jonathan <thaler.jonathan@gmail.com>
2018-07-14 17:31:00 +0100

    renamed SugarScapeSTM to indicate its use of TVar, will implement TArray version soon

--- public/stmabs/code/SugarScape/SugarScapeSTMTVar/src/Main.hs
+++ public/stmabs/code/SugarScape/SugarScapeSTMTVar/src/Main.hs
@@ -17,7 +17,7 @@ import           Simulation
 durationSecs :: Double
 durationSecs = 60
 
--- NOTE run with: clear & stack exec -- SugarScapeSTM +RTS -N4 -s
+-- NOTE run with: clear & stack exec -- SugarScapeSTMTVar +RTS -N4 -s
 
 main :: IO ()
 main = do

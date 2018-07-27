c818fd3740c8ff2ed47dc7c9b02756ea2e6fc98a
thalerjonathan <thaler.jonathan@gmail.com>
2018-07-27 16:23:35 +0100

    figure of agent scaling performance

--- writing/papers/workingdrafts/stmabs/results/sug/agents_syncenv.m
+++ writing/papers/workingdrafts/stmabs/results/sug/agents_syncenv.m
@@ -1,23 +1,17 @@
-stm = [52.8, 51.1, 62.2, 65.0];
-stm_retries = [0.002, 0.05, 0.03, 0.04];
-cores = [1, 2, 3, 4];
+sequential = [14.121, 6.841, 4.5, 3.302, 2.641];
+tvar = [21.1, 11.3, 8.1, 6.2, 5.2];
+tarray = [74.4, 56.8, 45.2, 37.0, 31.7];
+
+agents = [500, 1000, 1500, 2000, 2500];
 
 figure;
 hold on;
-yyaxis left
-plot(cores, stm, 'Color', [0, 0.7, 0]);
 
-yyaxis right
-plot (cores, stm_retries, 'Color', [0.7, 0.0, 0]);
+plot(agents, sequential, 'Color', [0.8, 0.0, 0], 'LineWidth', 2);
+plot(agents, tvar, 'Color', [0, 0, 0.8], 'LineWidth', 2);
+plot(agents, tarray, 'Color', [0, 0.8, 0], 'LineWidth', 2);
 
-legend ("STM", "STM Retries");
+legend ("Sequential", "TVar", "TArray");
+xlabel ("Agents");
+ylabel ("Steps per second");
 grid on;
-
-  	\begin{tabular}{ c || c | c | c }
-        Agents  & Sequential     & TVar       & TArray     \\ \hline \hline 
-    	500     & 14.121 (0.096) & 21.1 (0.2) & \textbf{74.4} (1.1) \\ \hline
-   		1,000   & 6.841 (0.092)  & 11.3 (0.3) & \textbf{56.8} (0.2) \\ \hline
-   		1,500   & 4.5 (0.116)    & 8.1 (0.03) & \textbf{45.2} (0.2) \\ \hline
-   		2,000   & 3.302 (0.07)   & 6.2 (0.01) & \textbf{37.0} (0.2) \\ \hline 
-   		2,500   & 2.641 (0.129)  &5.2 (0.02)  & \textbf{31.7} (0.3)
-   	\end{tabular}

f4ea545b5775496bf0ea43006e7794407e0fea88
Jonathan Thaler <thaler.jonathan@gmail.com>
2018-07-30 14:03:27 +0100

    added sugarscape pure impelmentation

---
+++ public/testingfpabs/code/sug/SugarScapePure/src/Environment.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE Arrows #-}
+module Environment 
+  (
+    cellUnoccupied
+  , cellOccupied
+  
+  , regrow
+  , sugEnvironment
+  ) where
+
+import Data.Maybe
+
+import Control.Monad.Random
+import Control.Monad.State.Strict
+import FRP.BearRiver
+
+import AgentMonad
+import Discrete
+import Model
+
+------------------------------------------------------------------------------------------------------------------------
+-- ENVIRONMENT-BEHAVIOUR
+------------------------------------------------------------------------------------------------------------------------
+cellOccupied :: SugEnvCell -> Bool
+cellOccupied cell = isJust $ sugEnvOccupier cell
+
+cellUnoccupied :: SugEnvCell -> Bool
+cellUnoccupied = not . cellOccupied
+
+regrowSugar :: RandomGen g
+            => Double 
+            -> StateT SugEnvironment (Rand g) ()
+regrowSugar rate
+    | rate < 0  = regrowSugarToMax
+    | otherwise = regrowSugarByRate
+  where
+    regrowSugarByRate :: RandomGen g
+                      => StateT SugEnvironment (Rand g) ()
+    regrowSugarByRate  
+      = updateCellsM (\c -> 
+        c { sugEnvSugarLevel = 
+              min
+                  (sugEnvSugarCapacity c)
+                  ((sugEnvSugarLevel c) + rate)}) -- if this bracket is omited it leads to a bug: all environment cells have +1 level
+
+    regrowSugarToMax :: StateT SugEnvironment (Rand g) ()
+    regrowSugarToMax = updateCellsM (\c -> c { sugEnvSugarLevel = sugEnvSugarCapacity c})
+
+regrow :: RandomGen g 
+       => StateT SugEnvironment (Rand g) ()
+regrow = regrowSugar sugarGrowbackUnits
+
+sugEnvironment :: RandomGen g 
+               => SugAgent g
+sugEnvironment = proc _ -> do
+  arrM_ (lift $ lift  regrow) -< ()
+  returnA -< agentOut
---
+++ public/testingfpabs/code/sug/SugarScapePure/src/Model.hs
@@ -0,0 +1,95 @@
+module Model 
+  (
+    SugAgentState (..)
+  , SugAgentObservable (..)
+
+  , SugEnvCellOccupier (..)
+  , SugEnvCell (..)
+
+  , SugAgentMonad
+  , SugAgentMonadT
+
+  , SugEnvironment
+
+  , SugAgent
+  , SugAgentDef
+  , SugAgentIn
+  , SugAgentOut
+
+  , sugarGrowbackUnits
+  , sugarCapacityRange
+  , sugarEndowmentRange
+  , sugarEndowmentRangeStandard
+  , sugarMetabolismRange
+  , visionRange
+  , visionRangeStandard
+  ) where
+
+import Control.Monad.Random
+import Control.Monad.State.Strict
+
+import AgentMonad
+import Discrete
+
+------------------------------------------------------------------------------------------------------------------------
+-- DOMAIN-SPECIFIC AGENT-DEFINITIONS
+------------------------------------------------------------------------------------------------------------------------
+data SugAgentState = SugAgentState 
+  { sugAgCoord            :: Discrete2dCoord
+  , sugAgSugarMetab       :: Double              -- this amount of sugar will be consumed by the agent in each time-step
+  , sugAgVision           :: Int                 -- the vision of the agent: strongly depends on the type of the environment: Int because its 2d discrete
+  , sugAgSugarLevel       :: Double              -- the current sugar holdings of the agent, if 0 then the agent starves to death
+  , sugAgSugarInit        :: Double              -- agent is fertile only when its sugarlevel is GE than its initial endowment
+    } deriving (Show)
+
+data SugAgentObservable = SugAgentObservable
+  { sugObsCoord    :: Discrete2dCoord
+  , sugObsVision   :: Int
+  } deriving (Show)
+
+data SugEnvCellOccupier = SugEnvCellOccupier 
+  { sugEnvOccId     :: AgentId
+  , sugEnvOccWealth :: Double
+  } deriving (Show)
+
+data SugEnvCell = SugEnvCell 
+  { sugEnvSugarCapacity :: Double
+  , sugEnvSugarLevel    :: Double
+  , sugEnvOccupier      :: Maybe SugEnvCellOccupier
+  } deriving (Show)
+
+type SugEnvironment = Discrete2d SugEnvCell
+
+type SugAgentMonad g  = StateT SugEnvironment (Rand g)
+type SugAgentMonadT g = AgentT (SugAgentMonad g)
+
+type SugAgent g     = Agent    (SugAgentMonad g) SugAgentObservable
+type SugAgentDef g  = AgentDef (SugAgentMonad g) SugAgentObservable
+type SugAgentIn     = AgentIn 
+type SugAgentOut g  = AgentOut (SugAgentMonad g) SugAgentObservable
+------------------------------------------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------------------------------------------------
+-- CHAPTER II: Life And Death On The Sugarscape
+------------------------------------------------------------------------------------------------------------------------
+-- NOTE: < 0 is treated as grow back to max
+sugarGrowbackUnits :: Double
+sugarGrowbackUnits = 1.0
+
+sugarCapacityRange :: (Double, Double)
+sugarCapacityRange = (0.0, 4.0)
+
+sugarEndowmentRange :: (Double, Double)
+sugarEndowmentRange = sugarEndowmentRangeStandard
+-- NOTE: this is specified in book page 33 where the initial endowments are set to 5-25
+sugarEndowmentRangeStandard :: (Double, Double)
+sugarEndowmentRangeStandard = (5.0, 25.0)
+
+sugarMetabolismRange :: (Double, Double)
+sugarMetabolismRange = (1.0, 5.0)
+
+visionRange :: (Int, Int)
+visionRange = visionRangeStandard
+-- NOTE: set to 1-6 on page 24
+visionRangeStandard :: (Int, Int)
+visionRangeStandard = (1, 6)

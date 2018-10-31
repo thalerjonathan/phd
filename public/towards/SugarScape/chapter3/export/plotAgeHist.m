e3395dfe2d0e35ebbd5586ff89b6e6662fa2e9e3
Jonathan Thaler <thaler.jonathan@gmail.com>
2018-10-31 08:22:54 +0000

    population dynamics are not exact like in sugarscape book but according to replication in NetLogo they couldnt do it as well, also our dynamics are qualitatively the same as the netlogo implementation

---
+++ public/towards/SugarScape/chapter3/export/plotAgeHist.m
@@ -0,0 +1,19 @@
+% plotting the last 
+t = length( dynamics );
+ages = [];
+
+for ai = 2 : length (dynamics{t})
+  a = dynamics{t}{ai};
+  agentId = dynamics{t}{ai}(1);
+  agentAge = dynamics{t}{ai}(2);
+  agentSug = dynamics{t}{ai}(3);
+  agentMet = dynamics{t}{ai}(4);
+  agentVis = dynamics{t}{ai}(5);
+  
+  ages(end+1,:) = agentAge;
+endfor
+  
+% needed for octave on Fedora, otherwise only black output
+graphics_toolkit("gnuplot");
+
+hist(ages);

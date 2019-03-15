b45dc3246a16398a340ca55db4daeeef329cd7db
Jonathan Thaler <thaler.jonathan@gmail.com>
2019-03-15 12:17:09 +0800

    trying to run agents in parallel

---
+++ thesis/code/parallel/SIR/SIR_Dunai/src/Main.hs
@@ -0,0 +1,430 @@
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE Strict #-}
+module Main where
+
+import           Data.IORef
+import           System.IO
+import           Text.Printf
+
+import           Control.Monad.Random
+import           Control.Monad.Reader
+import           Control.Monad.Trans.MSF.Random
+import           Data.Array.IArray
+import           FRP.BearRiver
+import qualified Graphics.Gloss as GLO
+import qualified Graphics.Gloss.Interface.IO.Animate as GLOAnim
+
+data SIRState = Susceptible | Infected | Recovered deriving (Show, Eq)
+
+type Disc2dCoord  = (Int, Int)
+type SIREnv       = Array Disc2dCoord SIRState
+
+type SIRMonad g   = Rand g
+type SIRAgent g   = SF (SIRMonad g) SIREnv SIRState
+
+type SimSF g = SF (SIRMonad g) () SIREnv
+
+data SimCtx g = SimCtx 
+  { simSf    :: !(SimSF g)
+  , simEnv   :: !SIREnv
+  , simRng   :: g
+  , simSteps :: !Integer
+  , simTime  :: !Time
+  }
+
+contactRate :: Double
+contactRate = 5.0
+
+infectivity :: Double
+infectivity = 0.05
+
+illnessDuration :: Double
+illnessDuration = 15.0
+
+agentGridSize :: (Int, Int)
+agentGridSize = (31, 31)
+
+winSize :: (Int, Int)
+winSize = (800, 800)
+
+winTitle :: String
+winTitle = "Agent-Based SIR on 2D Grid"
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+
+  let visualise = False
+      t         = 150
+      dt        = 0.1
+      seed      = 123 -- 123 -- 42 leads to recovery without any infection
+      
+      g         = mkStdGen seed
+      (as, env) = initAgentsEnv agentGridSize
+      sfs       = map (\(coord, a) -> (sirAgent coord a, coord)) as
+      sf        = simulationStep sfs env
+      ctx       = mkSimCtx sf env g 0 0
+
+  if visualise
+    then visualiseSimulation dt ctx
+    else do
+      --let ret = runSimulationUntil t dt ctx
+      --writeAggregatesToFile "SIR_DUNAI.m" ret 
+      writeSimulationUntil t dt ctx "SIR_DUNAI_dt001.m" 
+
+runSimulationUntil :: RandomGen g
+                   => Time
+                   -> DTime
+                   -> SimCtx g
+                   -> [(Double, Double, Double)]
+runSimulationUntil tMax dt ctx0 = runSimulationAux 0 ctx0 []
+  where
+    runSimulationAux :: RandomGen g
+                      => Time
+                      -> SimCtx g
+                      -> [(Double, Double, Double)]
+                      -> [(Double, Double, Double)]
+    runSimulationAux t ctx acc 
+        | t >= tMax = acc
+        | otherwise = runSimulationAux t' ctx' acc'
+      where
+        env  = simEnv ctx
+        aggr = aggregateStates $ elems env
+
+        t'   = t + dt
+        ctx' = runStepCtx dt ctx
+        acc' = aggr : acc
+
+writeSimulationUntil :: RandomGen g
+                     => Time
+                     -> DTime
+                     -> SimCtx g
+                     -> String
+                     -> IO ()
+writeSimulationUntil tMax dt ctx0 fileName = do
+    fileHdl <- openFile fileName WriteMode
+    hPutStrLn fileHdl "dynamics = ["
+    writeSimulationUntilAux 0 ctx0 fileHdl
+    hPutStrLn fileHdl "];"
+
+    writeMatlabPlot fileHdl dt 
+
+    hClose fileHdl
+  where
+    writeSimulationUntilAux :: RandomGen g
+                            => Time
+                            -> SimCtx g
+                            -> Handle
+                            -> IO ()
+    writeSimulationUntilAux t ctx fileHdl 
+        | t >= tMax = return ()
+        | otherwise = do
+          let env  = simEnv ctx
+              aggr = aggregateStates $ elems env
+  
+              t'   = t + dt
+              ctx' = runStepCtx dt ctx
+
+          hPutStrLn fileHdl (sirAggregateToString aggr)
+
+          writeSimulationUntilAux t' ctx' fileHdl
+     
+visualiseSimulation :: RandomGen g
+                    => DTime
+                    -> SimCtx g
+                    -> IO ()
+visualiseSimulation dt ctx0 = do
+    ctxRef <- newIORef ctx0
+
+    GLOAnim.animateIO
+      (GLO.InWindow winTitle winSize (0, 0))
+      GLO.white
+      (nextFrame ctxRef)
+      (const $ return ())
+
+  where
+    (cx, cy)   = agentGridSize
+    (wx, wy)   = winSize
+    cellWidth  = (fromIntegral wx / fromIntegral cx) :: Double
+    cellHeight = (fromIntegral wy / fromIntegral cy) :: Double
+
+    nextFrame :: RandomGen g
+              => IORef (SimCtx g)
+              -> Float 
+              -> IO GLO.Picture
+    nextFrame ctxRef _ = do
+      ctx <- readIORef ctxRef
+      
+      let ctx' = runStepCtx dt ctx
+      writeIORef ctxRef ctx'
+
+      return $ ctxToPic ctx
+
+    ctxToPic :: RandomGen g
+             => SimCtx g 
+             -> GLO.Picture
+    ctxToPic ctx = GLO.Pictures $ aps ++ [timeStepTxt]
+      where
+          env = simEnv ctx
+          as  = assocs env
+          aps = map renderAgent as
+          t   = simTime ctx
+
+          (tcx, tcy)  = transformToWindow (-7, 10)
+          timeTxt     = printf "%0.1f" t
+          timeStepTxt = GLO.color GLO.black $ GLO.translate tcx tcy $ GLO.scale 0.5 0.5 $ GLO.Text timeTxt
+
+    renderAgent :: (Disc2dCoord, SIRState) -> GLO.Picture
+    renderAgent (coord, Susceptible) 
+        = GLO.color (GLO.makeColor 0.0 0.0 0.7 1.0) $ GLO.translate x y $ GLO.Circle (realToFrac cellWidth / 2)
+      where
+        (x, y) = transformToWindow coord
+    renderAgent (coord, Infected)    
+        = GLO.color (GLO.makeColor 0.7 0.0 0.0 1.0) $ GLO.translate x y $ GLO.ThickCircle 0 (realToFrac cellWidth)
+      where
+        (x, y) = transformToWindow coord
+    renderAgent (coord, Recovered)   
+        = GLO.color (GLO.makeColor 0.0 0.70 0.0 1.0) $ GLO.translate x y $ GLO.ThickCircle 0 (realToFrac cellWidth)
+      where
+        (x, y) = transformToWindow coord
+
+    transformToWindow :: Disc2dCoord -> (Float, Float)
+    transformToWindow (x, y) = (x', y')
+      where
+        rw = cellWidth
+        rh = cellHeight
+
+        halfXSize = fromRational (toRational wx / 2.0)
+        halfYSize = fromRational (toRational wy / 2.0)
+
+        x' = fromRational (toRational (fromIntegral x * rw)) - halfXSize
+        y' = fromRational (toRational (fromIntegral y * rh)) - halfYSize
+
+mkSimCtx :: RandomGen g
+         => SimSF g
+         -> SIREnv
+         -> g
+         -> Integer
+         -> Time
+         -> SimCtx g
+mkSimCtx sf env g steps t = SimCtx {
+    simSf    = sf
+  , simEnv   = env
+  , simRng   = g
+  , simSteps = steps
+  , simTime  = t
+  }
+
+runStepCtx :: RandomGen g
+           => DTime
+           -> SimCtx g
+           -> SimCtx g
+runStepCtx dt ctx = ctx'
+  where
+    g   = simRng ctx
+    sf  = simSf ctx
+
+    sfReader            = unMSF sf ()
+    sfRand              = runReaderT sfReader dt
+    ((env, simSf'), g') = runRand sfRand g
+
+    steps = simSteps ctx + 1
+    t     = simTime ctx + dt
+    ctx'  = mkSimCtx simSf' env g' steps t
+
+initAgentsEnv :: (Int, Int) -> ([(Disc2dCoord, SIRState)], SIREnv)
+initAgentsEnv (xd, yd) = (as, e)
+  where
+    xCenter = floor $ fromIntegral xd * (0.5 :: Double)
+    yCenter = floor $ fromIntegral yd * (0.5 :: Double)
+    
+    sus = [ ((x, y), Susceptible) | x <- [0..xd-1], 
+                                    y <- [0..yd-1],
+                                    x /= xCenter ||
+                                    y /= yCenter ] 
+    inf = ((xCenter, yCenter), Infected)
+    as = inf : sus
+
+    e = array ((0, 0), (xd - 1, yd - 1)) as
+
+simulationStep :: RandomGen g
+               => [(SIRAgent g, Disc2dCoord)]
+               -> SIREnv
+               -> SF (SIRMonad g) () SIREnv
+simulationStep sfsCoords env = MSF $ \_ -> do
+    let (sfs, coords) = unzip sfsCoords 
+
+    -- run all agents sequentially but keep the environment
+    -- read-only: it is shared as input with all agents
+    -- and thus cannot be changed by the agents themselves
+    -- run agents sequentially but with shared, read-only environment
+    ret <- mapM (`unMSF` env) sfs
+    -- construct new environment from all agent outputs for next step
+    let (as, sfs') = unzip ret
+        env' = foldr (\(coord, a) envAcc -> updateCell coord a envAcc) env (zip coords as)
+        
+        sfsCoords' = zip sfs' coords
+        cont       = simulationStep sfsCoords' env'
+    return (env', cont)
+  where
+    updateCell :: Disc2dCoord -> SIRState -> SIREnv -> SIREnv
+    updateCell c s e = e // [(c, s)]
+
+sirAgent :: RandomGen g => Disc2dCoord -> SIRState -> SIRAgent g
+sirAgent coord Susceptible = susceptibleAgent coord
+sirAgent _     Infected    = infectedAgent
+sirAgent _     Recovered   = recoveredAgent
+
+susceptibleAgent :: RandomGen g => Disc2dCoord -> SIRAgent g
+susceptibleAgent _coord
+    = switch 
+      -- delay the switching by 1 step, otherwise could
+      -- make the transition from Susceptible to Recovered within time-step
+      (susceptible >>> iPre (Susceptible, NoEvent))
+      (const infectedAgent)
+  where
+    susceptible :: RandomGen g 
+                => SF (SIRMonad g) SIREnv (SIRState, Event ())
+    susceptible = proc env -> do
+      makeContact <- occasionally (1 / contactRate) () -< ()
+
+      if not $ isEvent makeContact 
+        then returnA -< (Susceptible, NoEvent)
+        else (do
+          --let ns = neighbours env coord agentGridSize moore
+          let ns = allNeighbours env
+          s <- drawRandomElemS -< ns
+          case s of
+            Infected -> do
+              infected <- arrM_ (lift $ randomBoolM infectivity) -< ()
+              if infected 
+                then returnA -< (Infected, Event ())
+                else returnA -< (Susceptible, NoEvent)
+            _       -> returnA -< (Susceptible, NoEvent))
+
+infectedAgent :: RandomGen g => SIRAgent g
+infectedAgent
+    = switch
+      -- delay the switching by 1 step, otherwise could
+      -- make the transition from Susceptible to Recovered within time-step
+      (infected >>> iPre (Infected, NoEvent))
+      (const recoveredAgent)
+  where
+    infected :: RandomGen g => SF (SIRMonad g) SIREnv (SIRState, Event ())
+    infected = proc _ -> do
+      recovered <- occasionally illnessDuration () -< ()
+      if isEvent recovered
+        then returnA -< (Recovered, Event ())
+        else returnA -< (Infected, NoEvent)
+
+recoveredAgent :: RandomGen g => SIRAgent g
+recoveredAgent = arr (const Recovered) 
+
+drawRandomElemS :: MonadRandom m => SF m [a] a
+drawRandomElemS = proc as -> do
+  r <- getRandomRS ((0, 1) :: (Double, Double)) -< ()
+  let len = length as
+  let idx = fromIntegral len * r
+  let a =  as !! floor idx
+  returnA -< a
+
+randomBoolM :: RandomGen g => Double -> Rand g Bool
+randomBoolM p = getRandomR (0, 1) >>= (\r -> return $ r <= p)
+
+neighbours :: SIREnv 
+           -> Disc2dCoord 
+           -> Disc2dCoord
+           -> [Disc2dCoord] 
+           -> [SIRState]
+neighbours e (x, y) (dx, dy) n = map (e !) nCoords'
+  where
+    nCoords  = map (\(x', y') -> (x + x', y + y')) n
+    nCoords' = filter (\(nx, ny) -> nx >= 0 && 
+                                    ny >= 0 && 
+                                    nx <= (dx - 1) &&
+                                    ny <= (dy - 1)) nCoords
+allNeighbours :: SIREnv -> [SIRState]
+allNeighbours = elems
+
+neumann :: [Disc2dCoord]
+neumann = [ topDelta, leftDelta, rightDelta, bottomDelta ]
+
+moore :: [Disc2dCoord]
+moore = [ topLeftDelta,    topDelta,     topRightDelta,
+          leftDelta,                     rightDelta,
+          bottomLeftDelta, bottomDelta,  bottomRightDelta ]
+
+topLeftDelta :: Disc2dCoord
+topLeftDelta      = (-1, -1)
+topDelta :: Disc2dCoord
+topDelta          = ( 0, -1)
+topRightDelta :: Disc2dCoord
+topRightDelta     = ( 1, -1)
+leftDelta :: Disc2dCoord
+leftDelta         = (-1,  0)
+rightDelta :: Disc2dCoord
+rightDelta        = ( 1,  0)
+bottomLeftDelta :: Disc2dCoord
+bottomLeftDelta   = (-1,  1)
+bottomDelta :: Disc2dCoord
+bottomDelta       = ( 0,  1)
+bottomRightDelta :: Disc2dCoord
+bottomRightDelta  = ( 1,  1)
+
+writeAggregatesToFile :: String 
+                      -> DTime
+                      -> [(Double, Double, Double)] 
+                      -> IO ()
+writeAggregatesToFile fileName dt dynamics = do
+  fileHdl <- openFile fileName WriteMode
+  hPutStrLn fileHdl "dynamics = ["
+  mapM_ (hPutStrLn fileHdl . sirAggregateToString) dynamics
+  hPutStrLn fileHdl "];"
+
+  writeMatlabPlot fileHdl dt
+
+  hClose fileHdl
+
+writeMatlabPlot :: Handle 
+                -> DTime
+                -> IO ()
+writeMatlabPlot fileHdl dt = do
+  hPutStrLn fileHdl "susceptible = dynamics (:, 1);"
+  hPutStrLn fileHdl "infected = dynamics (:, 2);"
+  hPutStrLn fileHdl "recovered = dynamics (:, 3);"
+  hPutStrLn fileHdl "totalPopulation = susceptible(1) + infected(1) + recovered(1);"
+
+  hPutStrLn fileHdl "susceptibleRatio = susceptible ./ totalPopulation;"
+  hPutStrLn fileHdl "infectedRatio = infected ./ totalPopulation;"
+  hPutStrLn fileHdl "recoveredRatio = recovered ./ totalPopulation;"
+
+  hPutStrLn fileHdl "steps = length (susceptible);"
+  hPutStrLn fileHdl "indices = 0 : steps - 1;"
+  hPutStrLn fileHdl $ "indices = indices ./ " ++ show (1 / dt) ++ ";"
+
+  hPutStrLn fileHdl "figure"
+  hPutStrLn fileHdl "plot (indices, susceptibleRatio.', 'color', 'blue', 'linewidth', 2);"
+  hPutStrLn fileHdl "hold on"
+  hPutStrLn fileHdl "plot (indices, infectedRatio.', 'color', 'red', 'linewidth', 2);"
+  hPutStrLn fileHdl "hold on"
+  hPutStrLn fileHdl "plot (indices, recoveredRatio.', 'color', 'green', 'linewidth', 2);"
+
+  hPutStrLn fileHdl "set(gca,'YTick',0:0.05:1.0);"
+  
+  hPutStrLn fileHdl "xlabel ('Time');"
+  hPutStrLn fileHdl "ylabel ('Population Ratio');"
+  hPutStrLn fileHdl "legend('Susceptible','Infected', 'Recovered');"
+
+sirAggregateToString :: (Double, Double, Double) -> String
+sirAggregateToString (susceptibleCount, infectedCount, recoveredCount) =
+  printf "%f" susceptibleCount
+  ++ "," ++ printf "%f" infectedCount
+  ++ "," ++ printf "%f" recoveredCount
+  ++ ";"
+
+aggregateStates :: [SIRState] -> (Double, Double, Double)
+aggregateStates as = (susceptibleCount, infectedCount, recoveredCount)
+  where
+    susceptibleCount = fromIntegral $ length $ filter (Susceptible==) as
+    infectedCount = fromIntegral $ length $ filter (Infected==) as
+    recoveredCount = fromIntegral $ length $ filter (Recovered==) as
---
+++ thesis/code/parallel/SIR/SIR_Yampa/src/Main.hs
@@ -0,0 +1,281 @@
+{-# LANGUAGE Arrows #-}
+module Main where
+
+import System.IO
+import Text.Printf
+
+import Control.Monad.Random
+import FRP.Yampa
+
+import ParDpSwitch
+
+data SIRState = Susceptible | Infected | Recovered deriving (Show, Eq)
+type SIRAgent = SF [SIRState] SIRState
+
+contactRate :: Double
+contactRate = 5.0
+
+infectivity :: Double
+infectivity = 0.05
+
+illnessDuration :: Double
+illnessDuration = 15.0
+
+-- stack build --profile --flag Yampa:expose-core
+-- stack exec -- SIR-Yampa +RTS -p -h -s -N4
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+
+  let t    = 150
+      dt   = 0.1
+      seed = 42
+      g    = mkStdGen seed
+      
+      agentCount    = 1000
+      infectedCount = 1
+
+      as   = initAgents agentCount infectedCount
+      
+      --ass  = runSimulationUntil g t dt as
+      --dyns = aggregateAllStates ass
+
+  --writeAggregatesToFile "SIR_YAMPA.m" dyns
+  writeSimulationUntil g t dt as "SIR_YAMPA.m"
+
+runSimulationUntil :: RandomGen g 
+                   => g 
+                   -> Time 
+                   -> DTime 
+                   -> [SIRState]
+                   -> [[SIRState]]
+runSimulationUntil g0 t dt as = embed (stepSimulation sfs as) ((), dts)
+  where
+    steps = floor $ t / dt
+    dts   = replicate steps (dt, Nothing)
+
+    (rngs, _) = rngSplits g0 (length as) []
+    sfs       = zipWith sirAgent rngs as
+    
+writeSimulationUntil :: RandomGen g
+                     => g
+                     -> Time
+                     -> DTime
+                     -> [SIRState]
+                     -> String
+                     -> IO ()
+writeSimulationUntil g0 tMax dt as fileName = do
+    let (rngs, _) = rngSplits g0 (length as) []
+        sfs       = zipWith sirAgent rngs as
+
+    fileHdl <- openFile fileName WriteMode
+    hPutStrLn fileHdl "dynamics = ["
+
+    simHdl <- reactInit 
+                (return ()) 
+                (\_ _ outs -> do
+                  let aggr = aggregateStates outs
+                  hPutStrLn fileHdl (sirAggregateToString aggr)
+                  return True)
+                (stepSimulation sfs as)
+
+    writeSimulationUntilAux 0 simHdl
+    hPutStrLn fileHdl "];"
+
+    writeMatlabPlot fileHdl dt 
+
+    hClose fileHdl
+  where
+    writeSimulationUntilAux :: Time
+                            -> ReactHandle () [SIRState]
+                            -> IO ()
+    writeSimulationUntilAux t simHdl
+        | t >= tMax = return ()
+        | otherwise = do
+          _ <- react simHdl (dt, Nothing)
+          writeSimulationUntilAux (t + dt) simHdl
+
+rngSplits :: RandomGen g => g -> Int -> [g] -> ([g], g)
+rngSplits g 0 acc = (acc, g)
+rngSplits g n acc = rngSplits g'' (n - 1) (g' : acc)
+  where
+    (g', g'') = split g
+
+stepSimulation :: [SIRAgent] -> [SIRState] -> SF () [SIRState]
+stepSimulation sfs as =
+    -- dpSwitch
+    dpSwitchParEval  
+      (\_ sfs' -> map (\sf -> (as, sf)) sfs')
+      sfs
+      -- if we switch immediately we end up in endless switching, so always wait for 'next'
+      (switchingEvt >>> notYet) 
+      stepSimulation
+
+  where
+    switchingEvt :: SF ((), [SIRState]) (Event [SIRState])
+    switchingEvt = arr (\(_, newAs) -> Event newAs)
+
+sirAgent :: RandomGen g => g -> SIRState -> SIRAgent
+sirAgent g Susceptible = susceptibleAgent g
+sirAgent g Infected    = infectedAgent g
+sirAgent _ Recovered   = recoveredAgent
+
+susceptibleAgent :: RandomGen g => g -> SIRAgent
+susceptibleAgent g0 = 
+    switch 
+      -- delay the switching by 1 step, otherwise could
+      -- make the transition from Susceptible to Recovered within time-step
+      (susceptible g0 >>> iPre (Susceptible, NoEvent))
+      (const $ infectedAgent g0)
+  where
+    susceptible :: RandomGen g => g -> SF [SIRState] (SIRState, Event ())
+    susceptible g = proc as -> do
+      makeContact <- occasionally g (1 / contactRate) () -< ()
+
+      -- NOTE: strangely if we are not splitting all if-then-else into
+      -- separate but only a single one, then it seems not to work,
+      -- dunno why
+      if isEvent makeContact
+        then (do
+          a <- drawRandomElemSF g -< as
+          case a of
+            Infected -> do
+              i <- randomBoolSF g infectivity -< ()
+              if i
+                then returnA -< (Infected, Event ())
+                else returnA -< (Susceptible, NoEvent)
+            _       -> returnA -< (Susceptible, NoEvent))
+        else returnA -< (Susceptible, NoEvent)
+
+infectedAgent :: RandomGen g => g -> SIRAgent
+infectedAgent g = 
+    switch 
+      -- delay the switching by 1 step, otherwise could
+      -- make the transition from Susceptible to Recovered within time-step
+      (infected >>> iPre (Infected, NoEvent))
+      (const recoveredAgent)
+  where
+    infected :: SF [SIRState] (SIRState, Event ())
+    infected = proc _ -> do
+      recEvt <- occasionally g illnessDuration () -< ()
+      let a = event Infected (const Recovered) recEvt
+      returnA -< (a, recEvt)
+
+recoveredAgent :: SIRAgent
+recoveredAgent = arr (const Recovered)
+
+randomBoolSF :: RandomGen g => g -> Double -> SF () Bool
+randomBoolSF g p = proc _ -> do
+  r <- noiseR ((0, 1) :: (Double, Double)) g -< ()
+  returnA -< (r <= p)
+
+drawRandomElemSF :: RandomGen g => g -> SF [a] a
+drawRandomElemSF g = proc as -> do
+  r <- noiseR ((0, 1) :: (Double, Double)) g -< ()
+  let len = length as
+  let idx = fromIntegral len * r
+  let a =  as !! floor idx
+  returnA -< a
+
+initAgents :: Int -> Int -> [SIRState]
+initAgents n i = sus ++ inf
+  where
+    sus = replicate (n - i) Susceptible
+    inf = replicate i Infected
+
+aggregateAllStates :: [[SIRState]] -> [(Double, Double, Double)]
+aggregateAllStates = map aggregateStates
+
+aggregateStates :: [SIRState] -> (Double, Double, Double)
+aggregateStates as = (susceptibleCount, infectedCount, recoveredCount)
+  where
+    susceptibleCount = fromIntegral $ length $ filter (Susceptible==) as
+    infectedCount = fromIntegral $ length $ filter (Infected==) as
+    recoveredCount = fromIntegral $ length $ filter (Recovered==) as
+
+writeAggregatesToFile :: String 
+                      -> DTime
+                      -> [(Double, Double, Double)] 
+                      -> IO ()
+writeAggregatesToFile fileName dt dynamics = do
+  fileHdl <- openFile fileName WriteMode
+  hPutStrLn fileHdl "dynamics = ["
+  mapM_ (hPutStrLn fileHdl . sirAggregateToString) dynamics
+  hPutStrLn fileHdl "];"
+
+  writeMatlabPlot fileHdl dt
+
+  hClose fileHdl
+
+writeMatlabPlot :: Handle 
+                -> DTime
+                -> IO ()
+writeMatlabPlot fileHdl dt = do
+  hPutStrLn fileHdl "susceptible = dynamics (:, 1);"
+  hPutStrLn fileHdl "infected = dynamics (:, 2);"
+  hPutStrLn fileHdl "recovered = dynamics (:, 3);"
+  hPutStrLn fileHdl "totalPopulation = susceptible(1) + infected(1) + recovered(1);"
+
+  hPutStrLn fileHdl "susceptibleRatio = susceptible ./ totalPopulation;"
+  hPutStrLn fileHdl "infectedRatio = infected ./ totalPopulation;"
+  hPutStrLn fileHdl "recoveredRatio = recovered ./ totalPopulation;"
+
+  hPutStrLn fileHdl "steps = length (susceptible);"
+  hPutStrLn fileHdl "indices = 0 : steps - 1;"
+  hPutStrLn fileHdl $ "indices = indices ./ " ++ show (1 / dt) ++ ";"
+
+  hPutStrLn fileHdl "figure"
+  hPutStrLn fileHdl "plot (indices, susceptibleRatio.', 'color', 'blue', 'linewidth', 2);"
+  hPutStrLn fileHdl "hold on"
+  hPutStrLn fileHdl "plot (indices, infectedRatio.', 'color', 'red', 'linewidth', 2);"
+  hPutStrLn fileHdl "hold on"
+  hPutStrLn fileHdl "plot (indices, recoveredRatio.', 'color', 'green', 'linewidth', 2);"
+
+  hPutStrLn fileHdl "set(gca,'YTick',0:0.05:1.0);"
+  
+  hPutStrLn fileHdl "xlabel ('Time');"
+  hPutStrLn fileHdl "ylabel ('Population Ratio');"
+  hPutStrLn fileHdl "legend('Susceptible','Infected', 'Recovered');"
+
+sirAggregateToString :: (Double, Double, Double) -> String
+sirAggregateToString (susceptibleCount, infectedCount, recoveredCount) =
+  printf "%f" susceptibleCount
+  ++ "," ++ printf "%f" infectedCount
+  ++ "," ++ printf "%f" recoveredCount
+  ++ ";"
+
+-- NOTE: this code demonstrates that an agent could 
+-- indeed make the transition from Susceptible to
+-- Recovered within a single time-step because
+-- switch applies the SF to switch into immediately.
+-- Using delayed switch (dSwitch) does NOT help, it
+-- only delays the OBSERVATION but does also apply
+-- the new SF immediately, thus the solution is 
+-- to use iPre which delays by 1 step by
+-- 'consuming' the immediate switching 
+{-
+sirTest :: SIRAgent
+sirTest = switch
+            susceptible --  >>> iPre (Susceptible, NoEvent))
+            (const infected)
+  where
+    susceptible :: SF [SIRState] (SIRState, Event ())
+    susceptible = proc _ -> returnA -< (Infected, Event ())
+
+    infected :: SIRAgent
+    infected = switch
+            (infectedAux >>> iPre (Infected, NoEvent))
+            (const recovered)
+      where
+        infectedAux :: SF [SIRState] (SIRState, Event ())
+        infectedAux = proc _ -> returnA -< (Recovered, Event ())
+
+    recovered :: SIRAgent
+    recovered = arr (const Recovered)
+
+main :: IO ()
+main = do
+  let ret = embed (sirTest) ([], [(0.1, Nothing), (0.1, Nothing)])
+  print ret
+-}

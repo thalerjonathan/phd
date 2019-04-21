70ecc93a402f32bb831772e30cca7dbadb78d6ab
Jonathan Thaler <thaler.jonathan@gmail.com>
2019-04-21 11:32:55 +0800

    cleaned up agent spes

--- thesis/code/proptesting/sir_timedriven/src/test/AgentSpecs.hs
+++ thesis/code/proptesting/sir_timedriven/src/test/AgentSpecs.hs
@@ -10,41 +10,32 @@ import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 
 import SIR.SIR
+import SIRGenerators
 import StatsUtils
-
--- import Debug.Trace
 
 -- clear & stack test sir-time:sir-agentspecs-test
 
 main :: IO ()
 main = defaultMain $ testGroup "SIR Agent Specifications Tests" 
           [ 
-          --   QC.testProperty 
-          --     "Susceptible agents invariants"
-          --     prop_susceptible_invariants
+            QC.testProperty 
+              "Susceptible agents invariants"
+              prop_susceptible_invariants
 
-           QC.testProperty 
-               "Susceptible agent infected"
-               prop_susceptible_infected
+          , QC.testProperty 
+             "Infected agent invariants"
+             prop_infected_invariants
 
-          -- , QC.testProperty 
-          --    "Infected agent invariants"
-          --    prop_infected_invariants
-
-          -- , QC.testProperty 
-          --    "Recovered agent invariants"
-          --    prop_recovered_invariants
-          
-          -- , QC.testProperty 
-          --    "Infected agent mean illness duration t-test"
-          --    prop_infected_meanIllnessDuration_ttest
+          , QC.testProperty 
+             "Recovered agent invariants"
+             prop_recovered_invariants
           ]
 
 --------------------------------------------------------------------------------
 -- PROPERTIES
 --------------------------------------------------------------------------------
 prop_susceptible_invariants :: Property
-prop_susceptible_invariants = checkCoverage $ do  
+prop_susceptible_invariants = property $ do  
     -- NOTE: these parameteres can be varied arbitrarily and the test will
     -- still work. Also note that we have to fix the population instead of taking
     -- a random one, otherwise percentage of coverage would change in every
@@ -87,36 +78,6 @@ prop_susceptible_invariants = checkCoverage $ do
         infIdx = fromJust infIdxMay
         recIdx = fromJust recIdxMay
 
--- TODO: find formula for susceptible became infected
-prop_susceptible_infected :: Property
-prop_susceptible_infected = checkCoverage $ do  
-    let cor = 5    -- beta, contact rate
-        inf = 0.05 -- gamma, infectivity
-        ild = 1.0  -- delta, illness duration NOTE: doesn't matter in this test, set to 1
-      
-    let as = [Susceptible, Infected, Infected, Recovered]
-
-    let i = length (filter (==Infected) as)
-        n = length as
-
-    let dt = 0.01
-
-    -- use CDF to get probability that up to contactRate contacts have occurred
-    -- because making contact events are distributed with the exponential distribution
-    let expContactProb = 100 * expCDF (1 / cor) cor
-        infectedRatio  = if n == 0 then 0 else fromIntegral i / fromIntegral n
-
-    -- compute expected probability
-    let expProb  = expContactProb * inf * infectedRatio
-
-    -- run a random susceptible agent for 1.0 time-unit and return true if infected
-    aos <- genSusceptible cor inf ild as 1 dt
-
-    -- expect given percentage of Susceptible agents to have become infected
-    return $ cover expProb (Infected `elem` aos) 
-           ("susceptible agents became infected, expected at least " 
-             ++ printf "%.2f" expProb ++ "%") True
-
 prop_infected_invariants :: Property
 prop_infected_invariants = checkCoverage $ do
     let illnessDuration = 15.0 -- delta, illnes duration
@@ -145,36 +106,6 @@ prop_infected_invariants = checkCoverage $ do
         then Just (dt * fromIntegral recIdx)
         else Nothing
 
-prop_infected_meanIllnessDuration_ttest:: Property
-prop_infected_meanIllnessDuration_ttest = checkCoverage $ do
-    let illnessDuration = 15.0 -- delta
-    -- run 100 random infected agents until they recover and return the 
-    -- duration they were ill
-    ids <- vectorOf 100 (genInfectedIllnessDuration illnessDuration)
-    
-    let confidence = 0.95
-        idsTTest   = tTestSamples TwoTail illnessDuration (1 - confidence) ids
-
-    return $ cover 95 (fromMaybe True idsTTest) 
-             ("infected agents have a mean illness duration of " ++ show illnessDuration) True
-  where
-    genInfectedIllnessDuration :: Double -> Gen Double
-    genInfectedIllnessDuration illnessDuration = do
-      g  <- genStdGen
-      ss <- listOf genSIRState
-
-      let a   = infectedAgent illnessDuration g
-          dt  = 0.5 
-          dts = repeat (dt, Nothing)
-          aos = embed a (ss, dts)
-      
-      -- we are searching in a potentially infinite list...
-      let mrec = elemIndex Recovered aos
-      -- ... t hus when elemIndex returns it will have Just
-      let recIdx = fromJust mrec 
-
-      return $ dt * fromIntegral recIdx
-
 prop_recovered_invariants :: Property
 prop_recovered_invariants = property $ do
   let dt = 0.01
@@ -189,14 +120,6 @@ prop_recovered_invariants = property $ do
 --------------------------------------------------------------------------------
 -- CUSTOM GENERATORS
 --------------------------------------------------------------------------------
-genStdGen :: Gen StdGen
-genStdGen = do
-  seed <- choose (minBound, maxBound)
-  return $ mkStdGen seed
-
-genSIRState :: Gen SIRState 
-genSIRState = elements [Susceptible, Infected, Recovered]
-
 genAgent :: (StdGen -> SIRAgent)
          -> [SIRState]
          -> Double
@@ -239,3 +162,67 @@ genRecovered :: [SIRState]
 genRecovered as tMax dt = do
   let a = const recoveredAgent
   genAgent a as tMax dt
+
+--------------------------------------------------------------------------------
+-- OBSOLETE TESTS
+--------------------------------------------------------------------------------
+-- NOTE: this does not work because it follows a bi-modal probability
+-- prop_susceptible_infected :: Property
+-- prop_susceptible_infected = checkCoverage $ do  
+--     let cor = 5    -- beta, contact rate
+--         inf = 0.05 -- gamma, infectivity
+--         ild = 1.0  -- delta, illness duration NOTE: doesn't matter in this test, set to 1
+      
+--     let as = [Susceptible, Infected, Recovered]
+
+--     let i = length (filter (==Infected) as)
+--         n = length as
+
+--     let dt = 0.01
+
+--     -- use CDF to get probability that up to contactRate contacts have occurred
+--     -- because making contact events are distributed with the exponential distribution
+--     let expContactProb = 100 * expCDF (1 / cor) cor
+--         infectedRatio  = if n == 0 then 0 else fromIntegral i / fromIntegral n
+
+--     -- compute expected probability
+--     let expProb  = expContactProb * inf * infectedRatio
+
+--     -- run a random susceptible agent for 1.0 time-unit and return true if infected
+--     aos <- genSusceptible cor inf ild as 1 dt
+
+--     -- expect given percentage of Susceptible agents to have become infected
+--     return $ cover expProb (Infected `elem` aos) 
+--            ("susceptible agents became infected, expected at least " 
+--              ++ printf "%.2f" expProb ++ "%") True
+
+-- NOTE: this is covered already by infected invariant 
+-- prop_infected_meanIllnessDuration_ttest:: Property
+-- prop_infected_meanIllnessDuration_ttest = checkCoverage $ do
+--     let illnessDuration = 15.0 -- delta
+--     -- run 100 random infected agents until they recover and return the 
+--     -- duration they were ill
+--     ids <- vectorOf 100 (genInfectedIllnessDuration illnessDuration)
+    
+--     let confidence = 0.95
+--         idsTTest   = tTestSamples TwoTail illnessDuration (1 - confidence) ids
+
+--     return $ cover 95 (fromMaybe True idsTTest) 
+--              ("infected agents have a mean illness duration of " ++ show illnessDuration) True
+--   where
+--     genInfectedIllnessDuration :: Double -> Gen Double
+--     genInfectedIllnessDuration illnessDuration = do
+--       g  <- genStdGen
+--       ss <- listOf genSIRState
+
+--       let a   = infectedAgent illnessDuration g
+--           dt  = 0.5 
+--           dts = repeat (dt, Nothing)
+--           aos = embed a (ss, dts)
+      
+--       -- we are searching in a potentially infinite list...
+--       let mrec = elemIndex Recovered aos
+--       -- ... t hus when elemIndex returns it will have Just
+--       let recIdx = fromJust mrec 
+
+--       return $ dt * fromIntegral recIdx
